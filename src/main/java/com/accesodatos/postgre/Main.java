1fee703ec30e01cca6afa57297146f711caac3c5
mvarela <mvarela@infol08.sanclemente.local>
2020-02-06 11:12:34 +0100

    Feito.

--- src/main/java/com/accesodatos/postgre/Main.java
+++ src/main/java/com/accesodatos/postgre/Main.java
@@ -1,18 +1,15 @@
 package com.accesodatos.postgre;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 import java.util.Properties;
+import org.postgresql.PGConnection;
+import org.postgresql.PGNotification;
 
 public class Main {
 
@@ -31,88 +28,97 @@ public class Main {
         //Dirección de conexión a base de datos
         String postgres = "jdbc:postgresql://"+url+"/"+db;
         
+        //Tempo en minutos que estara a espera
+        Integer tempo = 2;
+        
+        //Tempo que espera para cada consulta en milisegundos
+        Integer espera = 1000;
+        
         //Conectamos a base de datos
         try {
             Connection conn = DriverManager.getConnection(postgres,props);
             
-            //Creamos a táboa que conterá as imaxes
-            //NOTA: nón é moi lóxico crear funcións dende código. Só o fago para despois utilizala
+            //Creamos a táboa que conterá as mensaxes dos usarios
             String sqlTableCreation = new String(
-                "CREATE TABLE IF NOT EXISTS imaxes (nomeimaxe text, img bytea);");
+                "CREATE TABLE IF NOT EXISTS mensaxes ("
+                        + "id SERIAL PRIMARY KEY, "
+                        + "usuario TEXT NOT NULL, "
+                        + "mensaxes TEXT NOT NULL);");
+            CallableStatement createTable = conn.prepareCall(sqlTableCreation);
+            createTable.execute();
+            createTable.close();
 
-            //Executamos a sentencia SQL anterior
-            CallableStatement createFunction = conn.prepareCall(sqlTableCreation);
+            //Creamos a función que notificará que se engadiu unha nova mensaxe
+            String sqlCreateFunction = new String(
+                "CREATE OR REPLACE FUNCTION notificar_mensaxe() "+
+                "RETURNS trigger AS $$ "+
+                "BEGIN " +
+                    "PERFORM pg_notify('novamensaxe',NEW.id::text); "+
+                "RETURN NEW; "+
+                "END; "+
+                "$$ LANGUAGE plpgsql; ");
+            CallableStatement createFunction = conn.prepareCall(sqlCreateFunction);
             createFunction.execute();
             createFunction.close();
+
+            //Creamos o triguer que se executa tras unha nova mensaxe
+            String sqlCreateTrigger = new String(
+                "DROP TRIGGER IF EXISTS not_nova_mensaxe ON mensaxes; "+
+                "CREATE TRIGGER not_nova_mensaxe "+
+                "AFTER INSERT "+
+                "ON mensaxes "+
+                "FOR EACH ROW "+
+                "EXECUTE PROCEDURE notificar_mensaxe(); ");
+            CallableStatement createTrigger = conn.prepareCall(sqlCreateTrigger);
+            createTrigger.execute();
+            createTrigger.close();
+       
+            //Configuramos para estar a escoita
+            PGConnection pgconn = conn.unwrap(PGConnection.class);
+            Statement stmt = conn.createStatement();
+            stmt.execute("LISTEN novamensaxe");
+            stmt.close();
+            System.out.println("Esperando mensaxes...");
             
-            //Collemos o arquivo
-            String nomeFicheiro = new String("logo.png");
-            File file = new File(nomeFicheiro);
-            FileInputStream fis = new FileInputStream(file);
+            //Variables para controlar o tempo de espera
+            boolean flag=true;
+            Date date = new Date();
+            long initial = date.getTime();
             
-            //Creamos a consulta que inserta a imaxe na base de datos
-            String sqlInsert = new String(
-                "INSERT INTO imaxes VALUES (?, ?);");
-            PreparedStatement ps = conn.prepareStatement(sqlInsert);
-            
-            //Engadimos como primeiro parámetro o nome do arquivo
-            ps.setString(1, file.getName());
-            
-            //Engadimos como segundo parámetro o arquivo e a súa lonxitude
-            ps.setBinaryStream(2, fis, (int)file.length());
-            
-            //Executamos a consulta
-            ps.executeUpdate();
-            
-            //Cerrramos a consulta e o arquivo aberto
-            ps.close();
-            fis.close();
-            
-            //Creamos a consulta para recuperar a imaxe anterior
-            String sqlGet = new String(
-                "SELECT img FROM imaxes WHERE nomeimaxe = ?;");
-            PreparedStatement ps2 = conn.prepareStatement(sqlGet); 
-            
-            //Engadimos o nome da imaxe que queremos recuperar
-            ps2.setString(1, nomeFicheiro); 
-            
-            //Executamos a consulta
-            ResultSet rs = ps2.executeQuery();
-            
-            //Imos recuperando todos os bytes das imaxes
-            byte[] imgBytes = null;
-            while (rs.next()) 
-            { 
-                imgBytes = rs.getBytes(1); 
+            //Bucle para ir lendo as mensaxes
+            while(flag){
+                
+                //Collemos todas as notificacions
+                PGNotification notifications[] = pgconn.getNotifications();
+                
+                //Se hai notificacions, recorrémolas e imprimimos os parametros 
+                if(notifications != null){
+                    for(int i=0;i < notifications.length;i++){
+                        System.out.println(notifications[i].getParameter());
+                    }
+                }
+                
+                //Esperamos un pouco de tempo entre conexións
+                Thread.sleep(espera);
+                
+                //Comrpobamos se pasou o tempo de espera
+                if((initial + (tempo * 60000)) > new Date().getTime()) flag=false;
+                
             }
             
-            //Cerramos a consulta
-            rs.close(); 
-            ps2.close();
+            //deixamos de escoitar conexións
+            Statement stmt2 = conn.createStatement();
+            stmt2.execute("UNLISTEN novamensaxe");
+            stmt2.close();
             
-            //Creamos o fluxo de datos para gardar o arquivo recuperado
-            String ficheiroSaida = new String("logo2.png");
-            File fileOut = new File(ficheiroSaida);
-            FileOutputStream fluxoDatos = new FileOutputStream(fileOut);
             
-            //Gardamos o arquivo recuperado
-            if(imgBytes != null){
-                fluxoDatos.write(imgBytes);
-            }
-            
-            //cerramos o fluxo de datos de saida
-            fluxoDatos.close();    
-                        
-            //Cerramos a conexión coa base de datos
             if(conn!=null) conn.close();        
         
         } catch (SQLException ex) {
             System.err.println("Error: " + ex.toString());
-        } catch (FileNotFoundException ex) {
+        } catch (InterruptedException ex) {
             System.err.println("Error: " + ex.toString());
-        } catch (IOException ex) {
-            System.err.println("Error: " + ex.toString());
-        }       
+        }     
         
     }
     
